c8f346f597136aacfbe3f0b8b56e2330270b4526
Pablo <berrotaran.p@gmail.com>
2020-02-22 00:47:02 -0300

    First unit tests added

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,26 @@ impl ProgramSettings {
 		Ok(ProgramSettings { query, filename })
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn valid_program_settings() {
+		let args = vec![String::from(""), String::from("test_query"), String::from("test_filename.txt")];
+
+		let program_settings = ProgramSettings::new(&args).unwrap();
+
+		assert_eq!(&program_settings.query, "test_query");
+		assert_eq!(&program_settings.filename, "test_filename.txt");
+	}
+
+/*	#[test]
+	fn invalid_program_settings() {
+		let args = vec![String::from("")];
+		let program_settings = ProgramSettings::new(&args);
+
+		assert_eq!(program_settings, Err("Not enough parameters were passed") );
+	}*/
+}
